fc2a9d17692e41ad339264fa561e226b801a5a25
NamanS-2005 <2005namvan+github@gmail.com>
2025-07-26 01:19:12 +0530

    fixing remaining bugs

--- app/profile/[id]/page.js
+++ app/profile/[id]/page.js
@@ -8,21 +8,22 @@ import Profile from "@/components/Profile"
 
 const UserProfile = ({params}) => {
   const searchParams = useSearchParams()
+  const paramss = React.use(params)
   const userName = searchParams.get('name')
 
   const [userPosts, setUserPosts] = useState([])
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${params?.id}/posts`)
+      const response = await fetch(`/api/users/${paramss?.id}/posts`)
       const data = await response.json()
 
       setUserPosts(data)
     }
 
-    if(params?.id) fetchPosts()
+    if(paramss?.id) fetchPosts()
  
-  }, [params.id])
+  }, [paramss.id])
   
 
   return (
