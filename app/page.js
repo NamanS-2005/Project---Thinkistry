3e41810eb0be47429de72caed38bfd8e0500f830
NamanS-2005 <2005namvan+github@gmail.com>
2025-07-30 22:46:23 +0530

    Slight change

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <br />
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
-      <p className="desc text-center">Prompt-idea is an open source AI prompting tool for modern world to discover, create and share creative prompts</p>
+      <p className="desc text-center">Thinkistry is an open source AI prompting tool for modern world to discover, create and share creative prompts</p>
 
       <Feed/>
 
